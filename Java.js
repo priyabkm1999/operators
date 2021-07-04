e57f116100d0d1dc9db581238609f1e2fd6a36cf
priyabkm1999 <85467691+priyabkm1999@users.noreply.github.com>
2021-07-04 17:29:35 +0530

    adding files

---
+++ Java.js
@@ -0,0 +1,52 @@
+//-------------operators in javascript-------------
+//-------------Assignment operators----------------
+/*let x=3;
+{console.log(x)}
+{console.log(x+=3)}
+{console.log(x-=3)}
+{console.log(x*=3)}
+{console.log(x/=3)}
+{console.log(x%=3)}
+{console.log(x**=3)}*/
+//-------------Arithmetic operators----------------
+/*let x=4;
+let y=3;
+{console.log(x+y)}
+{console.log(x-y)}
+{console.log(x*y)}
+{console.log(x/y)}
+{console.log(x%y)}
+{console.log(x++)} //same for y
+{console.log(++x)} //same for y
+{console.log(x--)} //same for y
+{console.log(--x)} //same for y
+{console.log(x**y)}*/
+//-------------Comparison operators----------------
+/*let x=3;
+let y=5;
+{console.log(x==y)}
+{console.log(x!=y)}
+{console.log(x===y)}
+{console.log(x!==y)}
+{console.log(x>y)}
+{console.log(x>=y)}
+{console.log(x<y)}
+{console.log(x<=y)}*/
+//-------------Logical operators-------------------
+/*let x=5;
+let y=8;
+console.log(x>y && x<y)
+console.log(x>y || x<y)
+console.log(x>!y)*/
+//-------------Bitwise operators-------------------
+/*let x=0;
+let y=1;
+{console.log(x&y)}
+{console.log(x|y)}
+{console.log(x^y)}
+{console.log(~x)}
+{console.log(x<<y)}
+(console.log(x>>y))
+{console.log(x>>>y)}*/
+//-------------String operators--------------------
+console.log('hello'+ '_' +'world');
